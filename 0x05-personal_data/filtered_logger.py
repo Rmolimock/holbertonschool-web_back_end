832696e5489e7b27467347dd3e8c594fa383d0f4
Rmolimock <495@holbertonschool.com>
2020-11-03 06:02:01 +0000

    logging in python

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+""" logging """
+
+import logging
+import mysql.connector
+import os
+import re
+from typing import List
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ init """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ format """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+def filter_datum(fields: List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
+    """ filter """
+    for f in fields:
+        found = f + "=.+?(?=abc)*\\" + ";"
+        ret = re.sub(found, f + "=" + redaction + separator, message)
+    return ret
+
+
+def get_logger() -> logging.Logger:
+    """ get logger """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(RedactingFormatter(PII_FIELDS))
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ get db """
+    ret = mysql.connector.connect(
+        user=os.environ.get("PERSONAL_DATA_DB_USERNAME", "root"),
+        password=os.environ.get("PERSONAL_DATA_DB_PASSWORD", ""),
+        host=os.environ.get("PERSONAL_DATA_DB_HOST", "localhost"),
+        database=os.environ.get("PERSONAL_DATA_DB_NAME"))
+    return ret
+
+
+if __name__ == '__main__':
+    """ main """
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM users")
+    for each in cursor:
+        string = ""
+        for k in row:
+            string += "{}={}; ".format(k, each[k])
+        print(string)
+    cursor.close()
+    db.close()
