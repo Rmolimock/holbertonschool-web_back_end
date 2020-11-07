b925731a5317bcddb53bf27f286892fd14326120
Rmolimock <495@holbertonschool.com>
2020-11-07 07:48:34 +0000

    misplaced files

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -20,6 +20,19 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+@app.errorhandler(403)
+def Forbidden(error) -> str:
+    """ Forbidden
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.errorhandler(401)
+def Unauthorized(error) -> str:
+    """ Unauthorized
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
