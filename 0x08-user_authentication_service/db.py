23049a4254eb056cd57d18860e1c865046b7048d
Rmolimock <495@holbertonschool.com>
2020-11-16 19:45:36 +0000

    comments everywhere

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -3,13 +3,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+from typing import TypeVar
 from user import Base, User
 
 
 class DB:
+    """ db class """
 
     def __init__(self):
+        """ init """
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
@@ -17,15 +19,15 @@ class DB:
 
     @property
     def _session(self):
+        """ create a session """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
-    def add_user(self, email: str, hashed_password: str) -> User:
-        """ add user to the db """
-        user = User(email=email, hashed_password=hashed_password)
-        session = self._session
-        session.add(user)
-        session.commit()
-        return user
+
+    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+        """ add a user to the db """
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
