7ca34fa741ca89db3d361b0f2c26e0229cdfc9a7
Rmolimock <495@holbertonschool.com>
2020-11-18 19:11:37 +0000

    validate user login

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -24,6 +24,16 @@ class Auth:
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """ validate user """
+        if not email or not password:
+            return
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+
 
 def _hash_password(password: str) -> str:
     """ create a salted hash for the password """
