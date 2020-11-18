0e5e8fb23589df98b5b9c2353fa131abfb791f7a
Rmolimock <495@holbertonschool.com>
2020-11-18 19:05:01 +0000

    find_user_by return type more explicit

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -35,7 +35,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """ add a user to the db """
         if not email or not hashed_password:
             return
