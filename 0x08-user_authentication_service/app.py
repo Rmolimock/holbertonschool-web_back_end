a88921a16ab6ccfd42fecf1014bb85a69d0c96a1
Rmolimock <495@holbertonschool.com>
2020-11-19 04:41:11 +0000

    route for logging in a user

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from db import DB
 from auth import Auth
 
@@ -25,6 +25,20 @@ def users():
     except Exception as e:
         # print(e)
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """ route for user login """
+    email, password = request.form.get('email'), request.form.get('password')
+    valid_login = auth.valid_login(email, password)
+    if valid_login:
+        session_id = auth.create_session(email)
+        response = jsonify({"email": f"{email}", "message": "logged in"})
+        response.set_cookie('session_id', session_id)
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
