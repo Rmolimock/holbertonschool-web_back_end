3190488292cecde8128dcfcca4f401cacc413cbe
Russell Molimock <38747720+Rmolimock@users.noreply.github.com>
2020-12-03 00:30:14 -0600

    Update 5-app.py

--- 0x0A-i18n/5-app.py
+++ 0x0A-i18n/5-app.py
@@ -8,6 +8,14 @@ app = Flask(__name__)
 babel = Babel(app)
 
 
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
 class Config:
     """ Configuring Babel """
     BABEL_DEFAULT_LOCALE = "en"
