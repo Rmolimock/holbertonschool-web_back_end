33003e05d64f637e5c9a25f3812c4b0df7304660
vagrant <vagrant@ubuntu1804.localdomain>
2021-01-06 18:46:56 +0000

    pycodestyle compliant

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,6 +4,7 @@ This module contains one function: concat()
 It uses type annotation.
 '''
 
+
 def concat(str1: str, str2: str) -> str:
     '''This function concatinates two strings
     and demonstrates use of type annotation'''
