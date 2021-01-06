4ab257399f10ebba0461ff44b1bf21c751dabb73
vagrant <vagrant@ubuntu1804.localdomain>
2021-01-06 18:48:42 +0000

    pycodestyle compliant

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -7,8 +7,11 @@ It uses type annotation.
 from typing import Mapping, Any, Union, TypeVar
 
 
+U1 = Union[TypeVar('T'), None]
+U2 = Union[Any, TypeVar('T')]
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[TypeVar('T'), None] = None) -> Union[Any, TypeVar('T')]:
+
+def safely_get_value(dct: Mapping, key: Any, default: U1 = None) -> U2:
     '''return a complex annotated type'''
     if k in dct:
         return dct[k]
