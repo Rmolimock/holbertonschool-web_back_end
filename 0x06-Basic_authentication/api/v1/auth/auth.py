3334cf14c8a66c5f37fce2f6dcc4fec40bcee8a6
vagrant <vagrant@ubuntu1804.localdomain>
2021-01-11 05:59:27 +0000

    fixed sync

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -1,63 +1,6 @@
 #!/usr/bin/env python3
 
 """
-This module contains one class, SessionAuth
-"""
-
-from api.v1.auth.auth import Auth
-import base64
-from typing import TypeVar
-from models.user import User
-
-
-class SessionAuth(Auth):
-    """ SessionAuth class """
-    user_id_by_session_id = {}
-
-    def __init__(self):
-        """ empty init for now """
-        pass
-
-    def create_session(self, user_id: str = None) -> str:
-        """ create and return a session id, given a user id """
-        from uuid import uuid4
-        if not user_id or not type(user_id) == str:
-            return None
-        self.id = str(uuid4())
-        # stored as {session_id: user_id, session_id: user_id}
-        SessionAuth.user_id_by_session_id[self.id] = user_id
-        return self.id
-
-    def user_id_for_session_id(self, session_id: str = None) -> str:
-        """ return a user id given a session id """
-        if not session_id or not type(session_id) == str:
-            return None
-        return SessionAuth.user_id_by_session_id.get(session_id)
-
-    def current_user(self, request=None):
-        """ return a user object from given session id """
-        session_id = self.session_cookie(request)
-        user_id = self.user_id_for_session_id(session_id)
-        if not user_id:
-            return None
-        return User.get(user_id)
-
-    def destroy_session(self, request=None):
-        """ logout from current session """
-        if not request:
-            return False
-        session_id = self.session_cookie(request)
-        if not session_id:
-            return False
-        user_id = self.user_id_for_session_id(session_id)
-        if not user_id:
-            return False
-        del SessionAuth.user_id_by_session_id[session_id]
-        return True
-Russells-MacBook-Pro:v1 russellmolimock$ cat auth/auth.py 
-#!/usr/bin/env python3
-
-"""
 This module contains one class, Auth
 """
 
